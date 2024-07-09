3f104ba7cff9e416c2a9e35f917ad2cf13a9c28d
Alen Šober <sober.alen.prog@gmail.com>
2024-07-09 22:51:26 +0200

    Solved Day-13 part1

--- examples/day13/main.py
+++ examples/day13/main.py
@@ -1,38 +1,44 @@
 import json
 
-# Funkcija, ki preverja posamezne elemente v različnih listih
-def compare_list(list1, list2):
-    for elem1 in list1:
-        for elem2 in list2:
-            if(elem1 == elem2):
-                continue
-            else:
-                print(f"False: {elem1} | {elem2}")      
-                return False
-    return True
+# Funkcija, ki preverja elemente seznama
+def compare_lists(list1, list2):
+    #print(f"List1: {list1} | List2: {list2}")
 
-# Funkcija, ki skrbi za preveranje celotnega lista
-def compare_nested_lists(list1, list2):
-    # Premikamo se skozi oba list-a
-    for i in range(min(len(list1), len(list2))):
-        elem1: list = list1[i]
-        elem2: list = list2[i]
+    # Preverimo, če sta oba elementa tipa int
+    if isinstance(list1, int) and isinstance(list2, int):
+        # Preverimo, če sta elementa enaka, če sta vrnemo "Same"
+        if list1 == list2:
+            return "Same"
+        # Če nista enaka vrnemo bool (True, False) glede na to, če je el1 manjši od el2 
+        else: 
+            return list1 < list2
 
-        # Preverimo, če je leva stran manjša
-        if(len(list1) < len(list2)):
-            print(f"List Smaller: {False}")
-            return False 
-        # Preverimo posamezne elemente
-        if not compare_list(elem1, elem2):
-            print(f"List Different: {False}")
-            return False
-    return True
+    # Preverimo, če sta oba elementa tipa list
+    if isinstance(list1, list) and isinstance(list2, list):
+        # Iteriramo skozi oba seznama hkrati z uporabo zip
+        for e1, e2 in zip(list1, list2):
+            # Za vsak par recurzivno pokličemo compare_lists
+            if (comparison := compare_lists(e1, e2)) is not "Same":
+                return comparison # Vrnemo, če rezultat ni Same
+        return compare_lists(len(list1), len(list2)) # Primerja dolžini seznamov in vrnemo rezultat primerjave dolžin
 
-# Tester:
-list1 = [[1], [4, 4, 4]]
-list2 = [[1], [4]]
+    # Če je list1 tipa int in list2 tipa list pretvorimo list1 v seznam z enim elementom in ponovno pokličemo compare_lists.
+    if isinstance(list1, int):
+        return compare_lists([list1], list2)
+    # Če je list2 tipa int in list1 tipa list pretvorimo list2 v seznam z enim elementom in ponovno pokličemo compare_lists.
+    else: 
+        return compare_lists(list1, [list2])
 
 pairs = [[json.loads(p) for p in e.splitlines()] for e in open(r'examples\day13\input.txt').read().split("\n\n")]
 
-result = compare_nested_lists(list1, list2)
-print(result)
+sum = 0
+for i, (left, right) in enumerate(pairs, 1):
+    #Če je levi del manjši od desnega se index para prišteje k spremenljivki sum
+    if compare_lists(left, right):
+        # Dodamo index v sum
+        sum += i
+
+# Izpišemo sum
+print(sum)
+
+
